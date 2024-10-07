c3dce318f17f66c85dca91ae94a319c5ded671b2
lakshya <uietcselakshya2024@kuk.ac.in>
2024-10-07 17:50:25 +0530

    push

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router()
-const { CreateUsers } = require('../controller/usercontroller')
+const multer = require('multer')
+const { CreateUsers, getAllUserData } = require('../controller/usercontroller')
 
-router.post('/Lakshya', CreateUsers)
+const upload = multer({ storage: multer.diskStorage({}), })
 
-router.all('/*',(req,res)=>{
-    return res.status(400).send({status:false,msg:'Invalid Url'})
+router.post('/CreateUsers', upload.single(),CreateUsers)
+router.get('/getAllUserData', getAllUserData)
+
+
+router.all('/*', (req, res) => {
+    return res.status(400).send({ status: false, msg: 'Invalid Url' })
 })
 
 module.exports = router;
